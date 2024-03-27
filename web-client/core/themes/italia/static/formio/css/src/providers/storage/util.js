cff6682740b0b17ca08179127efbcebd1148d72f
Alessio Gerace <alessio.gerace@gmail.com>
2024-03-27 12:04:39 +0100

    [FIX] client

--- web-client/core/themes/italia/static/formio/css/src/providers/storage/util.js
+++
@@ -1,6 +0,0 @@
-export async function withRetries(fn, args, retries = 3, err = null) {
-  if (!retries) {
-    throw new Error(err);
-  }
-  return fn(...args).catch(() => withRetries(fn, args, retries - 1, err));
-}
